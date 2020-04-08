b7449fe71e4baf4022784f107d1dabb68c218757
Andrew Horn <arhorn@smcm.edu>
2020-04-08 18:03:26 -0500

    SecondPage now works

--- src/pages/apitest.js
+++ src/pages/apitest.js
@@ -1,8 +1,70 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import { Link } from "gatsby"
+import axios from 'axios'
 import "./style.css"
 
 const SecondPage = () => {
+  const [issueList, setIssueList] = useState("")
+
+  const [newTitle, setNewTitle] = useState("")
+  const [newText, setNewText] = useState("")
+  const [newCreatedBy, setNewCreatedBy] = useState("")
+  const [newAssignedTo, setNewAssignedTo] = useState("")
+  const [newStatusText, setNewStatusText] = useState("")
+  const [postResponse, setPostResponse] = useState("")
+
+  useEffect( () => {    
+    (async function fetchData() {
+      try {
+        const response = await axios.get('http://localhost:5000/api/issues/apitest');
+        setIssueList(response.data)
+      } catch (e) {
+        console.log(e)
+      }
+    })()
+  });
+
+  const handlePostIssue = async (e) => {
+    e.preventDefault()
+    const response = await axios.post(
+      "http://localhost:5000/api/issues/apitest",
+      {
+        issue_title: newTitle,
+        issue_text: newText,
+        created_by: newCreatedBy,
+        assigned_to: newAssignedTo,
+        status_text: newStatusText,
+      }
+    )
+    setNewTitle("")
+    setNewText("")
+    setNewCreatedBy("")
+    setNewAssignedTo("")
+    setNewStatusText("")
+    setPostResponse(JSON.stringify(response.data))
+  }
+
+  const handleClose = async (e, _id, open) => {
+    e.preventDefault()
+    try {
+      await axios.put('http://localhost:5000/api/issues/apitest', {
+        '_id': _id,
+        'open': open
+      }) 
+    } catch (e) {
+      console.log(e)
+    }
+  } 
+
+  const handleDelete = async (e, _id) => {
+    e.preventDefault()
+    try {
+      await axios.delete(`http://localhost:5000/api/issues/apitest?_id=${_id}`)
+    } catch (e) {
+      console.log(e)
+    }
+  } 
+
   return (
     <div>
       <header>
@@ -18,7 +80,8 @@ const SecondPage = () => {
               name="issue_title"
               placeholder="*Title"
               style={{ width: "320px", marginBottom: "3px" }}
-              required=""
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
             />
             <br />
             <textarea
@@ -26,7 +89,8 @@ const SecondPage = () => {
               name="issue_text"
               placeholder="*Text"
               style={{ width: "320px", height: "100px" }}
-              required=""
+              value={newText}
+              onChange={(e) => setNewText(e.target.value)}
             />
             <br />
             <input
@@ -34,26 +98,53 @@ const SecondPage = () => {
               name="created_by"
               placeholder="*Created by"
               style={{ width: "100px" }}
-              required=""
+              value={newCreatedBy}
+              onChange={(e) => setNewCreatedBy(e.target.value)}
             />
             <input
               type="text"
               name="assigned_to"
               placeholder="(opt)Assigned to"
               style={{ width: "100px" }}
+              value={newAssignedTo}
+              onChange={(e) => setNewAssignedTo(e.target.value)}
             />
             <input
               type="text"
               name="status_text"
               placeholder="(opt)Status text"
               style={{ width: "100px" }}
+              value={newStatusText}
+              onChange={(e) => setNewStatusText(e.target.value)}
             />
             <br />
-            <button type="submit">Submit Issue</button>
+            <button type="submit" onClick={handlePostIssue}>Submit Issue</button>
           </form>
+          {postResponse && (
+          <div>
+            <p>
+              <b>Response:</b>
+            </p>
+            <code>{postResponse}</code>
+          </div>
+        )}
         </div>
 
-        <div id="issueDisplay"></div>
+        {
+          issueList && (
+            issueList.map((issue, key) => (
+              <div className={`issue ${issue.open || 'closed'}`} key={key}>
+                <p className='id'>{issue._id}</p>
+                <h3>{issue.issue_title} - {issue.open ? 'open' : 'closed'}</h3>
+                <p>{issue.issue_text}</p>
+                <p>{issue.status_text}</p>
+            <p className="id"><b>Created by:</b> {issue.created_by} <b>Assigned to:</b> {issue.assigned_to}</p>
+            <p className="id"><b>Created on:</b> {issue.created_on} <b>Last updated:</b> {issue.updated_on}</p>
+            <a href="#" onClick={(e) => handleClose(e, issue._id, !issue.open)}>{issue.open ? 'close' : 'open'}?</a> <a href="#" onClick={e => handleDelete(e, issue._id)}>delete?</a>
+              </div>
+            )).reverse()
+          )
+        }
 
         <hr style={{ margin: "50px", marginTop: "200px" }} />
       </center>
